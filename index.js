7af58f0663221562d2ee5c34d4c1b2a999e23a88
harunazakaria <Harunzy55@gmail.com.com>
2024-02-08 12:32:34 +0000

    add all the new changes

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
-//this function allow user to enter any of this words(rock, paper,scissors)
-function getUserChoice() {
-  let userInput = prompt('Enter Rock, Paper, or Scissors').toLowerCase();
-  if (
-    userInput === 'rock' ||
-    userInput === 'paper' ||
-    userInput === 'scissors'
-  ) {
-    return userInput;
-  } else {
-    console.log('Invalid choice. Please enter Rock, Paper, or Scissors.'); //give this error if non of the words areentereed
-    return getUserChoice();
-  }
-}
-// this function generate a random number and match to any of the words (rock, paper,scissors)
-function getComputerChoice() {
-  let choices = ['rock', 'paper', 'scissors'];
-  let randomIndex = Math.floor(Math.random() * 3);
-  return choices[randomIndex];
-}
-//this function compare the above two functions and determine a winner, loser or tie
-function determineWinner(userChoice, computerChoice) {
-  if (userChoice === computerChoice) {
-    return "It's a tie!";
+function playRound(playerSelection) {
+  let computerChoices = ['rock', 'paper', 'scissors'];
+  let computerSelection =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  let resultElement = document.querySelector('#container');
+  resultElement.textContent = 'Player chose: ' + playerSelection + '\n';
+  resultElement.textContent += 'Computer chose: ' + computerSelection + '\n';
+
+  if (playerSelection === computerSelection) {
+    resultElement.textContent += "It's a tie!";
   } else if (
-    (userChoice === 'rock' && computerChoice === 'scissors') ||
-    (userChoice === 'paper' && computerChoice === 'rock') ||
-    (userChoice === 'scissors' && computerChoice === 'paper')
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-    return 'You win!';
+    resultElement.textContent += 'You win!';
   } else {
-    return 'You lose!';
+    resultElement.textContent += 'You lose!';
   }
 }
-// this allows the game to play and report a winner, loser or tie
-function playGame() {
-  let userChoice = getUserChoice();
-  let computerChoice = getComputerChoice();
 
-  console.log('You chose: ' + userChoice);
-  console.log('Computer chose: ' + computerChoice);
+document.getElementById('rock').addEventListener('click', function () {
+  playRound('rock');
+});
 
-  console.log(determineWinner(userChoice, computerChoice));
-}
-playGame();
+document.getElementById('paper').addEventListener('click', function () {
+  playRound('paper');
+});
+
+document.getElementById('scissors').addEventListener('click', function () {
+  playRound('scissors');
+});
